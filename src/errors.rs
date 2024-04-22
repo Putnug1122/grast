2e7574b76ee67da748383b877978e60f01b93025
Putnug1122 <putnug1122@gmail.com>
2024-04-22 11:10:01 +0700

    add embedding errors and implementing content file parsing

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,36 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub struct SetupError(pub &'static str);
 impl std::error::Error for SetupError {}
 impl std::fmt::Display for SetupError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Error: {}", self.0)
+    }
+}
+
+#[derive(Debug)]
+pub struct EmbeddingError;
+
+impl std::error::Error for EmbeddingError {}
+impl Display for EmbeddingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Embedding error")
+    }
+}
+
+impl From<anyhow::Error> for EmbeddingError {
+    fn from(value: anyhow::Error) -> Self {
+        Self {}
+    }
+}
+
+#[derive(Debug)]
+pub struct NotAvailableError;
+
+impl std::error::Error for NotAvailableError {}
+impl std::fmt::Display for NotAvailableError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "File 'not available' error")
     }
 }
